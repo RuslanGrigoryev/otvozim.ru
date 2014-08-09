08785d67e645fa59869477df73c500bd7baa5f9c
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2014-08-10 00:09:03 +0300

    Добавил несколько секций(слайдер, отзывы)

--- js/build/production.js
+++ js/build/production.js
@@ -252,3 +252,92 @@ var sliderModule = function ( _parent ) {
             action      : action
         }
 }('.row-right').action();
+
+$(function () {
+
+    var tabLink = $('.tab__list__link'),
+        tabList = $('.tab__list__item');
+
+    tabLink.on('click', function (e) {
+        e.preventDefault();
+
+        tabList.removeClass('tab__list__item--active');
+        $(this).closest('.tab__list__item').addClass('tab__list__item--active');
+
+    });
+
+});
+
+// slider
+(function($){
+
+htmSlider = function() {
+    this._init = function(element, options) {
+        var defaults = {
+        slideWrap : $(element),
+        nextLink : $('.next-slide'),
+        prevLink : $('.prev-slide')        
+        },
+        settings = $.extend(defaults, options); 
+
+
+        var slideWidth = settings.slideWrap.find('.slide-item').outerWidth(),
+            newLeftPos = settings.slideWrap.position().left - slideWidth;
+   
+        settings.nextLink.click(function(){
+            if( settings.nextLink.attr('name') == 'next' ) {
+            
+                settings.nextLink.removeAttr('name');
+                
+                settings.slideWrap.animate({left: newLeftPos}, 500, function(){
+                    settings.slideWrap
+                        .find('.slide-item:first')
+                        .appendTo(settings.slideWrap)
+                        .parent()
+                        .css({'left': 0});
+                });
+                
+                setTimeout(function(){ settings.nextLink.attr('name','next') }, 600);
+            }
+        });
+
+        settings.prevLink.click(function(){
+            if( settings.prevLink.attr('name') == 'prev' ) {
+            
+                settings.prevLink.removeAttr('name');
+            
+                settings.slideWrap
+                    .css({'left': newLeftPos})
+                    .find('.slide-item:last')
+                    .prependTo(settings.slideWrap)
+                    .parent()
+                    .animate({left: 0}, 500);
+
+                setTimeout(function(){ settings.prevLink.attr('name','prev') }, 600);
+            }
+        }); 
+
+        function autoplay(){
+            settings.slideWrap.animate({left: newLeftPos}, 600, function(){
+                settings.slideWrap
+                    .find('.slide-item:first')
+                    .appendTo(settings.slideWrap)
+                    .parent()
+                    .css({'left': 0});
+            });
+        }
+
+        if(settings.slideWrap.hasClass('play')){
+            setInterval(autoplay(), 500);
+        }  
+    };
+};
+// Launch plugin
+$.fn.htmSlider = function( options ){
+  return this.each(function(){
+       $( this ).data( "htmSlider", new htmSlider()._init( this, options ) );
+   });
+};
+})(jQuery);
+// end slider
+    $('.slide-wrap').htmSlider();
